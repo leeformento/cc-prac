96f80c797483e96b07fe13d22a6c949a45418e54
Lee Len Formento <leebatef@gmail.com>
2019-01-08 17:25:30 -0500

    jan 7

---
+++ demo.py
@@ -0,0 +1,28 @@
+ #import the modules we need
+
+import random
+
+# welcome message
+# results = load_results()
+
+#---------------------------------------------------------------------------------------------------------------
+
+# Write a function that counts the number
+
+def vowel_count(string):
+    # check how many vowels this string has
+    # iterate through string
+    # create array of vowels to check against
+    vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    for x in string:
+        # check if this char exists in vowels
+        if char in vowels:
+            # count variable
+            count +=1
+
+        return count
+
+    print(vowel_count('Supercalifragilisticexpealadocious'))
+
+    # print(sts.argv)
+
