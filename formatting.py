c3fe4929f602d30e17d5b862931e62d1f37c8025
Lee Len Formento <leebatef@gmail.com>
2019-01-08 22:07:39 -0500

    added dates using module

--- formatting.py
+++ formatting.py
@@ -1,4 +1,6 @@
-default_names = ["Lee", "Alex", "Oreo"]
+import datetime
+
+default_names = ["Lee", "Alex", "oreo"]
 default_amounts = [213123, 89.22, 33]
 
 unf_message = """Hi {name}
@@ -12,14 +14,17 @@ Have a great day!
 - Madam Auring
 """
 
+
 def make_messages(names, amounts):
     messages = []
     if len(names) == len(amounts):
         i = 0
+        today = datetime.date.today()
+        text = '{today.month}/{today.day}/{today.year}'.format(today = today)
         for name in names:
             new_msg = unf_message.format(
                 name = name,
-                date = 'some date',
+                date = text,
                 total = amounts[i]
             )
         i += 1 # loop going thru accordingly
